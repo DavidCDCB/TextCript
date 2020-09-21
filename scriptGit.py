3eb0c6cac8695a401d8972aea43f3f8383723f93
DavidCDCB <cristian-cb1@hotmail.com>
2020-09-21 15:19:53 -0500

    alt8

--- scriptGit.py
+++ scriptGit.py
@@ -75,9 +75,9 @@ def acciones(opt):
 		os.system("git add .")
 		os.system("git commit -a -m '"+str(message)+"'")
 		limpiar()
-		os.system("git log --graph --decorate --all --abbrev-commit --pretty=format:'%h - %s -> %an - %cd'")
+		os.system("git log -3 --graph --decorate --all --abbrev-commit")
 		input()
-		
+			
 	if(opt is "3"):
 		limpiar()
 		os.system("git fetch")
@@ -97,10 +97,7 @@ def acciones(opt):
 		
 	if(opt is "5"):
 		limpiar()
-		if(sys.platform.startswith('linux')):
-			os.system("git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%Creset' --abbrev-commit")
-		else:
-			os.system("git log --graph --decorate --all --abbrev-commit --pretty=format:'%h - %s -> %an - %cd")
+		os.system('git log --graph --pretty=format:"%C(cyan)%h%Creset -> %an -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)%Creset" --abbrev-commit')
 		input()
 		limpiar()
 		print("\n Ramas:\n")
